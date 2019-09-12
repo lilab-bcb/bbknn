0eb2fba90526e0ce4cfe4a0a20361343b969a58e
Yiming Yang <yang.yihming@gmail.com>
2019-09-12 00:50:07 -0400

    return knn distances as well

--- bbknn/__init__.py
+++ bbknn/__init__.py
@@ -288,7 +288,8 @@ def bbknn(adata, batch_key='batch', approx=True, metric='angular', copy=False, *
 	adata.uns['neighbors']['params'] = {'n_neighbors': len(bbknn_out[0][0,:].data)+1, 'method': 'umap'}
 	adata.uns['neighbors']['distances'] = bbknn_out[0]
 	adata.uns['neighbors']['connectivities'] = bbknn_out[1]
-	adata.uns['neighbors']['indices'] = bbknn_out[2]
+	adata.uns['neighbors']['knn_indices'] = bbknn_out[2]
+	adata.uns['neighbors']['knn_distances'] = bbknn_out[3]
 	return adata if copy else None
 
 def bbknn_pca_matrix(pca, batch_list, neighbors_within_batch=3, n_pcs=50, trim=None, 
@@ -338,7 +339,7 @@ def bbknn_pca_matrix(pca, batch_list, neighbors_within_batch=3, n_pcs=50, trim=N
 	#skip trimming if set to 0, otherwise trim
 	if trim > 0:
 		cnts = trimming(cnts=cnts,trim=trim)
-	return (dist, cnts, knn_indices)
+	return (dist, cnts, knn_indices, knn_distances)
 
 def extract_cell_connectivity(adata, cell, key='extracted_cell_connectivity'):
 	'''
